3613180ddc66a494f2fb167785f286c6d6e879a8
Alibaba <abdulmanafaliu414@gmail.com>
2024-08-19 03:15:43 +0000

    Changes made

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className='header'>
+      <h2>devfinder</h2>
+      <img src="/src/assets/images/icon-moon.svg" alt="Theme Toggle Icon" />
+    </div>
+  );
+}
+
+export default Header;

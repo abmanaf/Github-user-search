57360a3b211a1a39592413d04bda1ad60a4f2829
Alibaba <abdulmanafaliu414@gmail.com>
2024-08-26 05:25:55 +0000

    minor changes

--- src/Component/SearchBar/SearchBar.jsx
+++ src/Component/SearchBar/SearchBar.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, { useState,useEffect } from 'react';
 //import { useContext } from 'react';
-//import ThemeContext from '../ThemeContext';
 
 function SearchBar({ setUser }) { 
     const [searchTerm, setSearchTerm] = useState("abmanaf");
